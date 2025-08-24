3e1b085786cac547e64cf86d9f9cccac992729a5
thuntc3402-jpg <thuntc3402@ut.edu.vn>
2025-08-24 17:10:45 +0700

    Update client.py

--- client.py
+++ client.py
@@ -78,3 +78,17 @@ self.lbl_result = tk.Label(self.card, text="Kết quả: ...", font=("Segoe UI",
 
         self.enable_move_buttons(False)
         self.root.protocol("WM_DELETE_WINDOW", self.on_close)
+ # Gradient background
+    def draw_gradient(self, canvas, color1, color2):
+        width, height = 500, 420
+        r1, g1, b1 = self.root.winfo_rgb(color1)
+        r2, g2, b2 = self.root.winfo_rgb(color2)
+        r_ratio = (r2 - r1) / height
+        g_ratio = (g2 - g1) / height
+        b_ratio = (b2 - b1) / height
+        for i in range(height):
+            nr = int(r1 + (r_ratio * i))
+            ng = int(g1 + (g_ratio * i))
+            nb = int(b1 + (b_ratio * i))
+            color = f"#{nr>>8:02x}{ng>>8:02x}{nb>>8:02x}"
+            canvas.create_line(0, i, width, i, fill=color)
